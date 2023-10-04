5481814718830759fc8c58c5c01484124cf80fab
arkhaledwebdev <arkhaledwebdev@gmail.com>
2023-10-04 14:57:42 +0300

    Add validation for inputs

--- scripts.js
+++ scripts.js
@@ -5,52 +5,78 @@ const passwordInput = document.getElementById("password");
 const confirmPasswordInput = document.getElementById("confirmPassword");
 const submitButton = document.getElementById("submit-button");
 
+const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+
+
 const errorInfo = document.querySelector(".error-info")
 
-fNameInput.addEventListener('input',checkFirstNameInput);
-lNameInput.addEventListener('input',checkLastNameInput);
-emailInput.addEventListener('input',)
+let password = '';
+let confirmPassword = '';
+fNameInput.addEventListener('input',checkNameInput);
+lNameInput.addEventListener('input',checkNameInput);
 
-function checkFirstNameInput(e){
+passwordInput.addEventListener('input', checkPassword);
+confirmPasswordInput.addEventListener('input', checkPassword);
 
+function checkNameInput(e){
     let name = e.target.value;
 
+    let nameInput;
+
+    if(e.target.name === 'fname'){
+        nameInput = fNameInput;
+    }
+
+    if(e.target.name === 'lname'){
+        nameInput = lNameInput;
+    }
+
     if(name.length >= 3 && name.length <= 50 ){
-        fNameInput.classList.add("name-valid")
-        fNameInput.classList.remove("name-error")
+        nameInput.classList.add("name-valid")
+        nameInput.classList.remove("name-error")
         explainErrorInfo(0);
         
     }
     else if(name.length === 0){
-        fNameInput.classList.remove("name-valid")
-        fNameInput.classList.remove("name-error")
+        nameInput.classList.remove("name-valid")
+        nameInput.classList.remove("name-error")
         explainErrorInfo(0);
     }
     else{
-        fNameInput.classList.remove("name-valid")
-        fNameInput.classList.add("name-error")
+        nameInput.classList.remove("name-valid")
+        nameInput.classList.add("name-error")
         explainErrorInfo(1);
     }
 }
 
-function checkLastNameInput(e){
+function checkPassword(e){
 
-    let name = e.target.value;
-
-    if(name.length >= 3 && name.length <= 50 ){
-        lNameInput.classList.add("name-valid")
-        lNameInput.classList.remove("name-error")
-        explainErrorInfo(0);
+    if(e.target.name === "password"){
+        password = e.target.value;
     }
-    else if(name.length === 0){
-        lNameInput.classList.remove("name-valid")
-        lNameInput.classList.remove("name-error")
-        explainErrorInfo(0);
+
+    if(e.target.name === "confirmPass"){
+        confirmPassword = e.target.value;
+    }
+
+    if(checkPasswordRegEx(password)){
+        if(password === confirmPassword){
+            passwordInput.classList.add("name-valid");
+            confirmPasswordInput.classList.add("name-valid");
+            passwordInput.classList.remove("name-error");
+            confirmPasswordInput.classList.remove("name-error");
+            explainErrorInfo(0);
+        }
+        else{
+            passwordInput.classList.remove("name-valid");
+            confirmPasswordInput.classList.remove("name-valid");
+            passwordInput.classList.add("name-error");
+            confirmPasswordInput.classList.add("name-error");
+            explainErrorInfo(3);
+        }
     }
     else{
-        lNameInput.classList.remove("name-valid")
-        lNameInput.classList.add("name-error")
-        explainErrorInfo(1);
+        explainErrorInfo(2);
     }
 }
 
@@ -62,5 +88,15 @@ function explainErrorInfo(num){
     if(num === 1){
         errorInfo.textContent = "Enter a name between 3 to 50 characters";
     }
+    if(num === 2){
+        errorInfo.textContent = "at least 8 characters (letter, number & special character)";
+    }
+    if(num === 3){
+        errorInfo.textContent = "Passwords don't match";
+    }
+}
 
+
+function checkPasswordRegEx(pass){
+    return PASSWORD_REGEX.test(pass);
 }
